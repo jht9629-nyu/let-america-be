fa1c02cae86a046167641d70735cd857aa9204c3
John Henry Thompson <jht9629@nyu.edu>
2025-02-14 12:23:17 -0500

    trim external scripts

---
+++ src/js/play.js
@@ -0,0 +1,116 @@
+//
+
+// Advance to the next line
+function advance_next_line() {
+  delta_next_line(1);
+}
+
+function line_next() {
+  delta_next_line(1);
+  my.scrollEnabled = 0;
+  my.focusEnabled = 1;
+}
+globalThis.line_next = line_next;
+
+function line_previous() {
+  delta_next_line(-1);
+  my.scrollEnabled = 0;
+  my.focusEnabled = 1;
+}
+
+function line_continue() {
+  my.scrollEnabled = 1;
+  my.focusEnabled = 0;
+  my.elineDelayCount = 0;
+}
+
+function focus_line() {
+  let el = my.elines[my.elineIndex];
+  let rt = el.getBoundingClientRect();
+  overlayElement(el);
+  let midWindow = window.innerHeight / 2;
+  if (rt.y < midWindow || rt.y > midWindow + my.lineHeight) {
+    let diff = rt.y - midWindow;
+    window.scrollBy(0, diff * 0.1);
+  }
+  send_current_line();
+}
+
+function delta_next_line(delta) {
+  my.last_elineIndex = my.elineIndex;
+  let ne = my.elines.length;
+  my.elineIndex = (my.elineIndex + delta + ne) % ne;
+  let no = my.overlayColors.length;
+  my.overlayColorsIndex = (my.overlayColorsIndex + delta + no) % no;
+  if (my.elineIndex) {
+    send_current_line();
+  }
+}
+
+function check_scroll_pause() {
+  if (!my.scrollPauseStart) {
+    return;
+  }
+  let now = Date.now();
+  let nowDiff = now - my.scrollPauseStart;
+  if (nowDiff > my.scrollPausePeriod) {
+    my.scrollEnabled = 1;
+    my.scrollPauseStart = 0;
+  }
+}
+
+function start_scroll_pause() {
+  my.scrollEnabled = 0;
+  my.scrollPausePeriod = 5000;
+  my.scrollPauseStart = Date.now();
+}
+
+function send_current_line() {
+  // console.log('send_current_line');
+  if (!my.elines) return;
+  let eln = my.elines[my.elineIndex];
+  let num = my.elineIndex + 1;
+  let text = eln.textContent;
+  let color = my.overlayColors[my.overlayColorsIndex];
+  if (!my.offscreen) {
+    send_lineInfo({ num, text, color });
+  }
+}
+
+// lineInfo = { num, text }
+function send_lineInfo(lineInfo) {
+  // console.log('send_lineInfo lineInfo', lineInfo);
+  // ipcRenderer.send('set-line-info', lineInfo);
+}
+
+function play_from_top_toggle() {
+  if (my.full_read_enabled) {
+    play_from_top_short();
+  } else {
+    play_from_top_long();
+  }
+}
+
+function play_from_top_short() {
+  console.log('play_from_top_short ', my.full_read_enabled);
+  play_from_top(my.scrollYTopShort);
+  my.full_read_enabled = 0;
+}
+
+function play_from_top_long() {
+  console.log('play_from_top_long ', my.full_read_enabled);
+  play_from_top(my.scrollYTopLong);
+  my.full_read_enabled = 1;
+}
+
+function play_from_top(ytop) {
+  // gc();
+  my.topRunCount++;
+  console.log('my.topRunCount', my.topRunCount);
+  window.scrollTo(0, ytop);
+  start_scroll_pause();
+  my.elineIndex = 0;
+  my.elineDelayCount = 0;
+  my.overlayColorsIndex = (my.overlayColorsIndex + 1) % my.overlayColors.length;
+  send_current_line();
+}
--- src/js/preload-scroll.js
+++ src/js/preload-scroll.js
@@ -1,8 +1,4 @@
 //
-// const { webFrame } = require('electron');
-// require('./setup_responder.cjs');
-// require('./overlayElement.cjs');
-// require('./mbase_report_status.cjs');
 
 let my = {};
 window.my = my;
@@ -13,48 +9,20 @@ my.qrCodeWidth = '25%';
 
 my.shortStopLineNum = 5;
 
-// my.scrollYTopShort = 670;
-// my.scrollYTopShort = 760;
-// my.scrollYTopShort = 580;
-// my.scrollYTopShort = 610;
 my.scrollYTopShort = 0;
 
-// my.scrollYTopLong = 616;
-// my.scrollYTopLong = 460;
 my.scrollYTopLong = 0;
-
-// my.scrollPeriod = 0.1; // * 0.75;
-// my.elineDelayPeriod = 30; // * 0.75;
-
-// my.scrollPeriod = 0.1 * 0.75;
-// my.elineDelayPeriod = 30 * 0.75;
 
 my.scrollPeriod = 0.1;
 my.elineDelayPeriod = 30 * 0.5;
 
-my.zoomFactorShort = 1.0;
-my.zoomFactorLong = 1.0;
-// my.zoomFactorShort = 1.4;
-// my.zoomFactorLong = 2.18;
-
-my.gcCount = 0;
+my.topRunCount = 0;
 my.margin = 32;
 my.overlayColors = ['rgba(255, 80, 80, 0.5)', 'rgba(255, 180, 60, 0.5)', 'rgba(60, 190, 70, 0.5)'];
 // my.overlayColors = ['rgba(255, 80, 80, 1.0)', 'rgba(255, 180, 60, 1.0)', 'rgba(60, 190, 70, 1.0)'];
 my.overlayColorsIndex = 0;
 
-// setup_responder();
-
-window.addEventListener('DOMContentLoaded', () => {
-  // setTimeout(setup_scroll, 1000);
-  // webFrame_setZoomFactor(my.zoomFactorShort);
-  // // webFrame_setZoomFactor(my.zoomFactorLong);
-  // let zoomFactor = webFrame_getZoomFactor();
-  // console.log('zoomFactor', zoomFactor);
-  // // mbase_report_status({ msg: 'Here!' });
-
-  setup_scroll();
-});
+window.addEventListener('DOMContentLoaded', setup_scroll);
 
 window.addEventListener('mouseup', function (event) {
   // console.log('mouseup clientX', event.clientX, 'clientY', event.clientY);
@@ -65,10 +33,10 @@ window.addEventListener('mouseup', function (event) {
 });
 
 function setup_scroll() {
-  //
-  console.log('setup_scroll my', my);
+  // console.log('setup_scroll my', my);
   console.log('setup_scroll window.location.href', window.location.href);
 
+  // click on navbar at top of page -> play_from_top_toggle
   let nv = document.querySelector('.navbar');
   nv.addEventListener('mouseup', function (event) {
     console.log('nv mouseup clientX', event.clientX, 'clientY', event.clientY);
@@ -76,13 +44,20 @@ function setup_scroll() {
   });
   my.navbar_div = nv;
 
-  let nls = document.querySelectorAll('.nav-link');
-  for (let index = 0; index < nls.length; index++) {
-    let ent = nls[index];
-    console.log(index, ent.href);
-    ent.remove();
+  // remove all a links, except for poets.org
+  rns = document.querySelectorAll('a');
+  for (let index = 0; index < rns.length; index++) {
+    let ent = rns[index];
+    // console.log(index, ent.href);
+    if (ent.href.endsWith('/index.html')) {
+      // direct poets.org link in header to real site
+      ent.href = 'https://poets.org/poem/let-america-be-america-again';
+    } else {
+      ent.remove();
+    }
   }
 
+  // click on langston's image -> play_from_top_short
   let ff = document.querySelector('.field .field--field_image');
   ff.addEventListener('mouseup', function (event) {
     console.log('ff mouseup clientX', event.clientX, 'clientY', event.clientY);
@@ -90,6 +65,7 @@ function setup_scroll() {
     play_from_top_short();
   });
 
+  // click on "Let America Be Ameria" title at top of page -> play_from_top_toggle
   let pt = document.querySelector('.poem__title');
   pt.addEventListener('mouseup', function (event) {
     console.log('pt mouseup clientX', event.clientX, 'clientY', event.clientY);
@@ -99,17 +75,18 @@ function setup_scroll() {
   let fi = document.querySelector('.field--field_image');
   my.authorImageDiv = fi;
 
-  // remove more by poet block at end of page
+  // remove "more by poet" block at end of page
   let mp = document.querySelector('#block-views-block-poems-more-by-poet');
   while (mp.firstChild) {
     mp.firstChild.remove();
   }
 
-  // Duplicate image on right to bottom
+  // Duplicate image on right to bottom of page
   let bz = document.querySelector('#block-stanza-content');
   let bi = document.querySelector('#block-nodesidebarfields');
   bz.appendChild(bi.cloneNode(true));
 
+  // remove some elements
   let be = document.querySelector('#block-poemadaysignupblock');
   be.remove();
 
@@ -125,19 +102,12 @@ function setup_scroll() {
   let he = document.querySelector('.hero');
   he.remove();
 
-  // let na = document.querySelector('.navbar');
-  // na.remove();
-
-  // let et = document.querySelector('.field--title');
-  // let nb = document.querySelector('.navbar-brand');
-  // nb.innerHTML = nb.textContent + '<br/>' + et.textContent;
-  // nb.style.fontSize = 'xx-large';
-  // my.topBox = nb;
-
+  // my.fieldBody is used to attach overlay hilighting current line
   let ar = document.querySelector('article');
   let fb = ar.querySelector('.field--body');
   my.fieldBody = fb.querySelector('p');
 
+  // array of the line elements
   my.elines = ar.querySelectorAll('.long-line');
 
   my.elineIndex = 0;
@@ -154,28 +124,24 @@ function setup_scroll() {
 }
 
 function scroll_track() {
-  //
   my.lastScrollY = window.scrollY;
-
   check_scroll_pause();
-
   if (my.focusEnabled) {
     focus_line();
     return;
   } else {
     check_line_hilite();
   }
-
-  if (!my.scrollEnabled) return;
+  if (!my.scrollEnabled) {
+    return;
+  }
   window.scrollBy(0, 1);
-
   let stopped = !my.full_read_enabled && my.elineIndex == my.shortStopLineNum - 1;
   if (stopped) {
     console.log('scroll_track stopped', stopped);
     // play_from_top();
     pause_short_read();
   }
-
   // the author image moving off top of screen triggers play from top
   // in short read, when view is two column,
   // For Langstons' "America..." this is line 8 of poem
@@ -190,9 +156,9 @@ function scroll_track() {
 }
 
 // pause at bottom of screen before playing from top
-//
 function pause_short_read() {
   console.log('pause_short_read my.paused_at_bottom', my.paused_at_bottom);
+  //
   if (my.paused_at_bottom) {
     check_scroll_pause();
     if (my.scrollEnabled) {
@@ -205,9 +171,8 @@ function pause_short_read() {
   start_scroll_pause();
 }
 
+// Keep up last hilite until starting from the top
 function check_line_hilite() {
-  //
-  // Keep up last hilite until starting from the top
   if (my.last_elineIndex == my.elines.length - 1) {
     let rt = my.elines[0].getBoundingClientRect();
     if (rt.y < 0) {
@@ -257,7 +222,7 @@ function check_line_hilite() {
     }
     console.log('check_line_hilite while end my.elineIndex', my.elineIndex);
     console.log('check_line_hilite fullScan', fullScan, 'wrapScan', wrapScan);
-    if (wrapScan) {
+    if (wrapScan && my.scrollEnabled) {
       play_from_top_short();
     }
   } else {
@@ -267,139 +232,4 @@ function check_line_hilite() {
     return;
   }
   advance_next_line();
-}
-
-// Advance to the next line
-function advance_next_line() {
-  delta_next_line(1);
-}
-
-function line_next() {
-  delta_next_line(1);
-  my.scrollEnabled = 0;
-  my.focusEnabled = 1;
-}
-globalThis.line_next = line_next;
-
-function line_previous() {
-  delta_next_line(-1);
-  my.scrollEnabled = 0;
-  my.focusEnabled = 1;
-}
-
-function line_continue() {
-  my.scrollEnabled = 1;
-  my.focusEnabled = 0;
-  my.elineDelayCount = 0;
-}
-
-function focus_line() {
-  let el = my.elines[my.elineIndex];
-  let rt = el.getBoundingClientRect();
-  overlayElement(el);
-  let midWindow = window.innerHeight / 2;
-  if (rt.y < midWindow || rt.y > midWindow + my.lineHeight) {
-    let diff = rt.y - midWindow;
-    window.scrollBy(0, diff * 0.1);
-  }
-  send_current_line();
-}
-
-function delta_next_line(delta) {
-  my.last_elineIndex = my.elineIndex;
-  my.elineIndex = (my.elineIndex + delta + my.elines.length) % my.elines.length;
-  my.overlayColorsIndex = (my.overlayColorsIndex + delta + my.overlayColors.length) % my.overlayColors.length;
-  if (my.elineIndex) {
-    send_current_line();
-  }
-}
-
-function check_scroll_pause() {
-  if (!my.scrollPauseStart) {
-    return;
-  }
-  let now = Date.now();
-  let nowDiff = now - my.scrollPauseStart;
-  if (nowDiff > my.scrollPausePeriod) {
-    my.scrollEnabled = 1;
-    my.scrollPauseStart = 0;
-  }
-}
-
-// my.overlayColors = ['rgba(255, 205, 50, 1.0)', 'red', 'green'];
-// my.overlayColors = ['rgba(255, 205, 50, 1.0)', 'rgba(255, 0, 0, 0.5)', 'rgba(0, 255, 0, 0.5)'];
-// Apple Finder window close-hide-max
-// let my.scrollYTop = 465;
-// let my.scrollYTop = 635;
-// window.innerWidth 520
-// my.lastScrollY;
-
-function webFrame_setZoomFactor(n) {
-  document.body.style.zoom = n;
-}
-
-function webFrame_getZoomFactor() {
-  return parseFloat(document.body.style.zoom);
-}
-
-function start_scroll_pause() {
-  my.scrollEnabled = 0;
-  my.scrollPausePeriod = 5000;
-  my.scrollPauseStart = Date.now();
-}
-
-function send_current_line() {
-  // console.log('send_current_line');
-  if (!my.elines) return;
-  let eln = my.elines[my.elineIndex];
-  let num = my.elineIndex + 1;
-  let text = eln.textContent;
-  let color = my.overlayColors[my.overlayColorsIndex];
-  if (!my.offscreen) {
-    send_lineInfo({ num, text, color });
-  }
-}
-
-// lineInfo = { num, text }
-function send_lineInfo(lineInfo) {
-  // console.log('send_lineInfo lineInfo', lineInfo);
-  // ipcRenderer.send('set-line-info', lineInfo);
-}
-
-function play_from_top_toggle() {
-  if (my.full_read_enabled) {
-    play_from_top_short();
-  } else {
-    play_from_top_long();
-  }
-}
-
-function play_from_top_short() {
-  console.log('play_from_top_short ', my.full_read_enabled);
-  // if (my.full_read_enabled) {
-  //   webFrame_setZoomFactor(my.zoomFactorShort);
-  // }
-  play_from_top(my.scrollYTopShort);
-  my.full_read_enabled = 0;
-}
-
-function play_from_top_long() {
-  console.log('play_from_top_long ', my.full_read_enabled);
-  // if (!my.full_read_enabled) {
-  //   webFrame_setZoomFactor(my.zoomFactorLong);
-  // }
-  play_from_top(my.scrollYTopLong);
-  my.full_read_enabled = 1;
-}
-
-function play_from_top(ytop) {
-  // gc();
-  my.gcCount++;
-  console.log('my.gcCount', my.gcCount);
-  window.scrollTo(0, ytop);
-  start_scroll_pause();
-  my.elineIndex = 0;
-  my.elineDelayCount = 0;
-  my.overlayColorsIndex = (my.overlayColorsIndex + 1) % my.overlayColors.length;
-  send_current_line();
 }
