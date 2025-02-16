cf0de398d6c42082f984c1d82f57408b10ff0440
John Henry Thompson <jht9629@nyu.edu>
2025-02-16 12:11:46 -0500

    Update overlayElement.js

--- src/js/overlayElement.js
+++ src/js/overlayElement.js
@@ -4,23 +4,23 @@ function overlayElement(elt) {
   // Create a new div element for the overlay
   if (!my.overlayBack) {
     my.overlayBack = document.createElement('div');
-    // my.fieldBody.prepend(my.overlayBack);
-    my.fieldBody.appendChild(my.overlayBack);
+    my.fieldBody.prepend(my.overlayBack);
+    // my.fieldBody.appendChild(my.overlayBack);
     my.overlayBack.style.position = 'fixed';
     my.overlayBack.style.pointerEvents = 'none'; // Ensures the overlay doesn't block clicks
     my.overlayBack.style.backgroundColor = 'white';
   }
   if (!my.overlay) {
     my.overlay = document.createElement('div');
-    // my.fieldBody.prepend(my.overlay);
-    my.fieldBody.appendChild(my.overlay);
+    // my.fieldBody.appendChild(my.overlay);
+    my.overlayBack.appendChild(my.overlay);
     my.overlay.style.position = 'fixed';
     my.overlay.style.pointerEvents = 'none'; // Ensures the overlay doesn't block clicks
   }
   if (!my.cloned) {
     my.cloned = elt.cloneNode(true);
-    // my.fieldBody.prepend(my.cloned);
-    my.fieldBody.appendChild(my.cloned);
+    // my.fieldBody.appendChild(my.cloned);
+    my.overlayBack.appendChild(my.cloned);
     my.cloned.style.position = 'fixed';
     my.cloned.style.pointerEvents = 'none';
   }
@@ -52,44 +52,3 @@ function overlayElement(elt) {
   my.cloned.style.width = `${width}px`;
   my.cloned.style.height = `${height}px`;
 }
-globalThis.overlayElement = overlayElement;
-
-// https://chatgpt.com/
-// create a DOM element that overlays a transparent color at a specified location on the window
-// overlay a transparent color at a specified location on the window
-function overlayAtPosition({ x, y, width, height }) {
-  // Create a new div element for the overlay
-  // ...
-}
-
-function createStatusDiv() {
-  if (!my.statusDiv) {
-    let w = window.innerWidth;
-    let h = 32;
-
-    let x = window.innerWidth - w;
-    let y = window.innerHeight - h;
-    let width = w;
-    let height = h;
-
-    my.statusDiv = document.createElement('div');
-    // document.body.appendChild(my.statusDiv);
-    // my.topBox.appendChild(my.statusDiv);
-
-    // my.statusDiv.style.top = `${y}px`;
-    my.statusDiv.style.left = `${x}px`;
-    my.statusDiv.style.width = `${width}px`;
-    my.statusDiv.style.height = `${height}px`;
-
-    my.statusDiv.style.zIndex = 10;
-    my.statusDiv.style.backgroundColor = 'black';
-    my.statusDiv.style.color = 'white';
-    my.statusDiv.style.fontSize = `${h}px`;
-    // my.statusDiv.style.textAlign = 'right';
-    my.statusDiv.textContent = 'Starting...';
-
-    console.log('window.scrollY', window.scrollY);
-    console.log('my.statusDiv', my.statusDiv);
-  }
-}
-globalThis.createStatusDiv = createStatusDiv;
